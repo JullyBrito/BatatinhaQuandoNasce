955535d0e004c804eacc32fd1f0753f0ddfde466
Jully Brito <jullyS2brito@gmail.com>
2017-06-02 16:42:28 -0300

    Completando slides

--- asstes/js/churros.js
+++ asstes/js/churros.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+//Professor, essa parte eu peguei no seu exemplo, mas não estou sabendo como usar a parte do css
+//Fiquei com duvida na parte div:eq(1)
+
+$("div:eq(1)").css("background-color","blue");
 
 $( function() {
     $("#datepicker").datepicker();
